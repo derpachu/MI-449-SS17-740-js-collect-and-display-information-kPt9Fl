b93056db9cbb775dbf3543423f84736e89556bee
derpachu <nomnom148>
2017-02-19 17:24:08 -0500

    condensed code and made all variables consistant

--- js/index.js
+++ js/index.js
@@ -3,42 +3,27 @@ var lNameIn = document.getElementById('lName')
 var desIn = document.getElementById('description')
 var emailIn = document.getElementById('email')
 var phoneIn = document.getElementById('phone')
-var paragraph = document.getElementById('generated_profile')
 
-var updateParagraph = function () {
+var updateProfile = function () {
+  var formattedProfile = document.getElementById('refinedProfile')
   var firstName = fNameIn.value
   var lastName = lNameIn.value
   var description = desIn.value
   var email = emailIn.value
   var phone = phoneIn.value
+  var unformattedProfile = document.getElementById('rawProfile')
 
-  paragraph.innerHTML = '<h1>Hi, my name is ' + firstName + ' ' + lastName + '!</h1><p>' + description + '</p><p>' +
+  formattedProfile.innerHTML = '<h1>Hi, my name is ' + firstName + ' ' + lastName + '!</h1><p>' + description + '</p><p>' +
   'If you\'re interested in a date, you can email me at <a href="mailto:' + email + '" target="_blank">' + email +
-  '</a> or give me a call at <a href=tel:' + phone + '" target="_blank">' + phone + '</a></p>'
+  '</a> or give me a call at <a href=tel:' + phone + '" target="_blank">' + phone + '</a>.</p>'
+
+  unformattedProfile.textContent = '<h1>Hi, my name is ' + firstName + ' ' + lastName + '!</h1><p>' + description + '</p><p>' +
+  'If you\'re interested in a date, you can email me at <a href="mailto:' + email + '" target="_blank">' + email +
+  '</a> or give me a call at <a href=tel:' + phone + '" target="_blank">' + phone + '</a>.</p>'
 }
 
-fNameIn.addEventListener('input', updateParagraph)
-lNameIn.addEventListener('input', updateParagraph)
-desIn.addEventListener('input', updateParagraph)
-emailIn.addEventListener('input', updateParagraph)
-phoneIn.addEventListener('input', updateParagraph)
-
-var rawPara = document.getElementById('rawProfile')
-
-var updaterawParagraph = function () {
-  var firstName = fNameIn.value
-  var lastName = lNameIn.value
-  var description = desIn.value
-  var email = emailIn.value
-  var phone = phoneIn.value
-
-  rawPara.textContent = '<h1>Hi, my name is ' + firstName + ' ' + lastName + '!</h1><p>' + description + '</p><p>' +
-  'If you\'re interested in a date, you can email me at <a href="mailto:' + email + '" target="_blank">' + email +
-  '</a> or give me a call at <a href=tel:' + phone + '" target="_blank">' + phone + '</a></p>'
-}
-
-fNameIn.addEventListener('input', updaterawParagraph)
-lNameIn.addEventListener('input', updaterawParagraph)
-desIn.addEventListener('input', updaterawParagraph)
-emailIn.addEventListener('input', updaterawParagraph)
-phoneIn.addEventListener('input', updaterawParagraph)
+fNameIn.addEventListener('input', updateProfile)
+lNameIn.addEventListener('input', updateProfile)
+desIn.addEventListener('input', updateProfile)
+emailIn.addEventListener('input', updateProfile)
+phoneIn.addEventListener('input', updateProfile)
